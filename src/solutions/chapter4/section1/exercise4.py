04ee8d7a5aef810ab7b15289bc99ffc964dc1155
Krzysztof Wojtas <kwojtas@gmail.com>
2024-02-22 14:41:35 +0100

    #59 matrix_add_recursive

---
+++ src/solutions/chapter4/section1/exercise4.py
@@ -0,0 +1,36 @@
+from book.data_structures import Matrix
+
+
+def matrix_add_recursive(A: Matrix, B: Matrix, C: Matrix, n: int) -> None:
+    """Recursively adds two square matrices and places the result in the third square matrix.
+
+    Implements:
+        Matrix-Add-Recursive
+
+    Args:
+        A: the first square matrix to add
+        B: the second square matrix to add
+        C: the matrix to store the matrix sum
+        n: the dimension of matrices A and B
+    """
+    if n == 1:
+        C[1, 1] = A[1, 1] + B[1, 1]
+        return
+    (A11, A12, A21, A22), (B11, B12, B21, B22), (C11, C12, C21, C22) = __partition_matrices(A, B, C, n)
+    matrix_add_recursive(A11, B11, C11, n // 2)
+    matrix_add_recursive(A12, B12, C12, n // 2)
+    matrix_add_recursive(A21, B21, C21, n // 2)
+    matrix_add_recursive(A22, B22, C22, n // 2)
+
+
+def __partition_matrices(A, B, C, n):
+    return __partition_matrix(A, n), \
+        __partition_matrix(B, n), \
+        __partition_matrix(C, n)
+
+
+def __partition_matrix(M, n):
+    return M.submatrix((1, n // 2), (1, n // 2)), \
+        M.submatrix((1, n // 2), (n // 2 + 1, n)), \
+        M.submatrix((n // 2 + 1, n), (1, n // 2)), \
+        M.submatrix((n // 2 + 1, n), (n // 2 + 1, n))
--- test/test_solutions/test_chapter4.py
+++ test/test_solutions/test_chapter4.py
@@ -7,6 +7,7 @@ from hypothesis.strategies import lists
 from book.data_structures import Matrix
 from solutions.chapter4.section1.exercise1 import matrix_multiply_recursive_general
 from solutions.chapter4.section1.exercise3 import matrix_multiply_recursive_by_copying
+from solutions.chapter4.section1.exercise4 import matrix_add_recursive
 from test_case import ClrsTestCase
 from test_util import create_matrix
 
@@ -51,3 +52,23 @@ class TestChapter4(ClrsTestCase):
         actual_product = create_matrix(numpy.matmul(elements1, elements2))
 
         self.assertEqual(C, actual_product)
+
+    @given(st.data())
+    def test_matrix_add_recursive(self, data):
+        k = data.draw(integers(min_value=0, max_value=4), label="Matrices dimension exponent")
+        n = 2 ** k
+        elements1 = data.draw(
+            lists(lists(integers(min_value=-1000, max_value=1000), min_size=n, max_size=n), min_size=n, max_size=n),
+            label="First matrix elements")
+        elements2 = data.draw(
+            lists(lists(integers(min_value=-1000, max_value=1000), min_size=n, max_size=n), min_size=n, max_size=n),
+            label="Second matrix elements")
+        A = create_matrix(elements1)
+        B = create_matrix(elements2)
+        C = Matrix(n, n)
+
+        matrix_add_recursive(A, B, C, n)
+
+        actual_sum = create_matrix(numpy.add(elements1, elements2))
+
+        self.assertEqual(C, actual_sum)
