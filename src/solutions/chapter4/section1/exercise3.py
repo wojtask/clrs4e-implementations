54187aa17bd6fc70fb5886f668bc982b5989c635
Krzysztof Wojtas <kwojtas@gmail.com>
2024-02-22 11:39:29 +0100

    #58 matrix_multiply_recursive_by_copying

---
+++ src/solutions/chapter4/section1/exercise3.py
@@ -0,0 +1,56 @@
+from book.data_structures import Matrix
+from util import range_of
+
+
+def matrix_multiply_recursive_by_copying(A: Matrix, B: Matrix, C: Matrix, n: int) -> None:
+    """Recursively multiplies two square matrices and adds the result to the third square matrix. At each recursive step
+    partitions the matrices by copying their elements to new submatrices.
+
+    Args:
+        A: the first square matrix to multiply
+        B: the second square matrix to multiply
+        C: the matrix to add the result of the matrix multiplication
+        n: the dimension of matrices A and B
+    """
+    if n == 1:
+        C[1, 1] += A[1, 1] * B[1, 1]
+        return
+    (A11, A12, A21, A22), (B11, B12, B21, B22), (C11, C12, C21, C22) = __partition_matrices_by_copying(A, B, C, n)
+    matrix_multiply_recursive_by_copying(A11, B11, C11, n // 2)
+    matrix_multiply_recursive_by_copying(A11, B12, C12, n // 2)
+    matrix_multiply_recursive_by_copying(A21, B11, C21, n // 2)
+    matrix_multiply_recursive_by_copying(A21, B12, C22, n // 2)
+    matrix_multiply_recursive_by_copying(A12, B21, C11, n // 2)
+    matrix_multiply_recursive_by_copying(A12, B22, C12, n // 2)
+    matrix_multiply_recursive_by_copying(A22, B21, C21, n // 2)
+    matrix_multiply_recursive_by_copying(A22, B22, C22, n // 2)
+    __merge_submatrices_by_copying(C, C11, C12, C21, C22, n)
+
+
+def __partition_matrices_by_copying(A, B, C, n):
+    return __partition_matrix_by_copying(A, n), \
+        __partition_matrix_by_copying(B, n), \
+        __partition_matrix_by_copying(C, n)
+
+
+def __partition_matrix_by_copying(M, n):
+    M11 = Matrix(n // 2, n // 2)
+    M12 = Matrix(n // 2, n // 2)
+    M21 = Matrix(n // 2, n // 2)
+    M22 = Matrix(n // 2, n // 2)
+    for i in range_of(1, to=n // 2):
+        for j in range_of(1, to=n // 2):
+            M11[i, j] = M[i, j]
+            M12[i, j] = M[i, n // 2 + j]
+            M21[i, j] = M[n // 2 + i, j]
+            M22[i, j] = M[n // 2 + i, n // 2 + j]
+    return M11, M12, M21, M22
+
+
+def __merge_submatrices_by_copying(M, M11, M12, M21, M22, n):
+    for i in range_of(1, to=n // 2):
+        for j in range_of(1, to=n // 2):
+            M[i, j] = M11[i, j]
+            M[i, n // 2 + j] = M12[i, j]
+            M[n // 2 + i, j] = M21[i, j]
+            M[n // 2 + i, n // 2 + j] = M22[i, j]
--- test/test_solutions/test_chapter4.py
+++ test/test_solutions/test_chapter4.py
@@ -6,6 +6,7 @@ from hypothesis.strategies import lists
 
 from book.data_structures import Matrix
 from solutions.chapter4.section1.exercise1 import matrix_multiply_recursive_general
+from solutions.chapter4.section1.exercise3 import matrix_multiply_recursive_by_copying
 from test_case import ClrsTestCase
 from test_util import create_matrix
 
@@ -26,6 +27,26 @@ class TestChapter4(ClrsTestCase):
         C = Matrix(n, n)
 
         matrix_multiply_recursive_general(A, B, C, n)
+
+        actual_product = create_matrix(numpy.matmul(elements1, elements2))
+
+        self.assertEqual(C, actual_product)
+
+    @given(st.data())
+    def test_matrix_multiply_recursive_by_copying(self, data):
+        k = data.draw(integers(min_value=0, max_value=4), label="Matrices dimension exponent")
+        n = 2 ** k
+        elements1 = data.draw(
+            lists(lists(integers(min_value=-1000, max_value=1000), min_size=n, max_size=n), min_size=n, max_size=n),
+            label="First matrix elements")
+        elements2 = data.draw(
+            lists(lists(integers(min_value=-1000, max_value=1000), min_size=n, max_size=n), min_size=n, max_size=n),
+            label="Second matrix elements")
+        A = create_matrix(elements1)
+        B = create_matrix(elements2)
+        C = Matrix(n, n)
+
+        matrix_multiply_recursive_by_copying(A, B, C, n)
 
         actual_product = create_matrix(numpy.matmul(elements1, elements2))
 
