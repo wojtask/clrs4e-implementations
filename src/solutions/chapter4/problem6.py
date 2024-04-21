84be3417584d7c2f1ea1b7ac48aa6adb813ef9c7
Krzysztof Wojtas <kwojtas@gmail.com>
2024-04-21 22:25:05 +0200

    #237 identify_good_chip

---
+++ src/solutions/chapter4/problem6.py
@@ -0,0 +1,51 @@
+from enum import Enum
+from enum import auto
+from random import random
+
+
+class ChipCondition(Enum):
+    GOOD = auto()
+    BAD = auto()
+
+
+class Chip:
+    def __init__(self, condition: ChipCondition) -> None:
+        self.condition = condition
+
+
+class BadChipStrategy(Enum):
+    COIN_FLIP = 0.5
+    ALWAYS_REPORT_GOOD = 1.0
+    ALWAYS_REPORT_BAD = 0.0
+
+    def report(self):
+        return ChipCondition.GOOD if random() < self.value else ChipCondition.BAD
+
+
+def identify_good_chip(chips: set[Chip], *, strategy: BadChipStrategy = BadChipStrategy.COIN_FLIP) -> Chip:
+    """Identifies a good chip in a set of chips containing more good than bad chips.
+
+    Args:
+        chips: A set of chips containing more good than bad chips.
+        strategy: A strategy that bad chips use when reporting on the condition of another chip.
+
+    Returns:
+        One of the good chips from the input set.
+    """
+    if len(chips) <= 2:
+        return chips.pop()
+    selected_chips = set()
+    while len(chips) > 1:
+        first, second = chips.pop(), chips.pop()
+        test_result = __test_chips(first, second, strategy=strategy)
+        if test_result == (ChipCondition.GOOD, ChipCondition.GOOD):
+            selected_chips.add(first)  # second is equally fine here
+    if len(chips) == 1 and len(selected_chips) % 2 == 0:
+        selected_chips.add(chips.pop())
+    return identify_good_chip(selected_chips, strategy=strategy)
+
+
+def __test_chips(first: Chip, second: Chip, *, strategy: BadChipStrategy) \
+        -> (ChipCondition, ChipCondition):
+    return (second.condition if first.condition == ChipCondition.GOOD else strategy.report(),
+            first.condition if second.condition == ChipCondition.GOOD else strategy.report())
