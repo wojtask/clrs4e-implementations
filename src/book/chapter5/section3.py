174436994d42d610633896785ba548caa355284b
Krzysztof Wojtas <kwojtas@gmail.com>
2024-05-04 02:20:29 +0200

    #15 randomized_hire_assistant

--- src/book/chapter5/section3.py
+++ src/book/chapter5/section3.py
@@ -1,6 +1,21 @@
+from book.chapter5.section1 import hire_assistant
 from book.data_structures import Array
 from solutions.chapter5.section1.exercise2 import random
 from util import range_of
+
+
+def randomized_hire_assistant(rank: Array, n: int) -> None:
+    """Randomized version of Hire-Assistant.
+
+    Implements:
+        Randomized-Hire-Assistant
+
+    Args:
+        rank: the ranks of the candidates (a permutation of {1, 2, ..., n})
+        n: the number of candidates
+    """
+    randomly_permute(rank, n)
+    hire_assistant(rank, n)
 
 
 def randomly_permute(A: Array, n: int) -> None:
--- test/test_book/test_chapter5.py
+++ test/test_book/test_chapter5.py
@@ -8,6 +8,7 @@ from hypothesis.strategies import integers
 from hypothesis.strategies import lists
 
 from book.chapter5.section1 import hire_assistant
+from book.chapter5.section3 import randomized_hire_assistant
 from book.chapter5.section3 import randomly_permute
 from test_case import ClrsTestCase
 from test_util import create_array
@@ -27,6 +28,29 @@ class TestChapter5(ClrsTestCase):
 
         with patch('sys.stdout', new=StringIO()) as fake_out:
             hire_assistant(rank, n)
+
+            actual_output = fake_out.getvalue().splitlines()
+            interviewed_candidates = [int(line.removeprefix(INTERVIEWING_PREFIX)) for line in actual_output if
+                                      line.startswith(INTERVIEWING_PREFIX)]
+            hired_candidates = [int(line.removeprefix(HIRING_PREFIX)) for line in actual_output if
+                                line.startswith(HIRING_PREFIX)]
+            self.assertEqual(len(interviewed_candidates), n)
+            self.assertIn(len(hired_candidates), range_of(1, to=n))
+            self.assertEqual(interviewed_candidates, list(range_of(1, to=n)))
+            self.assertEqual(hired_candidates, sorted(hired_candidates))
+            self.assertTrue(set(hired_candidates).issubset(set(interviewed_candidates)))
+
+    @given(st.data())
+    def test_randomized_hire_assistant(self, data):
+        n = data.draw(st.integers(min_value=1, max_value=10))
+        rank_elements = list(range_of(1, to=n))
+        shuffle(rank_elements)
+        rank = create_array(rank_elements)
+        INTERVIEWING_PREFIX = 'interviewing candidate '
+        HIRING_PREFIX = 'hiring candidate '
+
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            randomized_hire_assistant(rank, n)
 
             actual_output = fake_out.getvalue().splitlines()
             interviewed_candidates = [int(line.removeprefix(INTERVIEWING_PREFIX)) for line in actual_output if
