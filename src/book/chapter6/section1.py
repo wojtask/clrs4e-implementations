3c8fd2b54f8888d9b3dd97ff74bb223e54fd7fc7
Krzysztof Wojtas <kwojtas@gmail.com>
2024-10-01 23:44:24 +0200

    #16 max_heapify

--- src/book/chapter6/section1.py
+++ src/book/chapter6/section1.py
@@ -1,3 +1,6 @@
+from util import range_of
+
+
 def parent(i: int) -> int:
     """The index of the parent of a heap node.
 
@@ -41,3 +44,26 @@ def right(i: int) -> int:
         The index of the right child of the node at index i.
     """
     return 2 * i + 1
+
+
+def max_heapify(A, i: int) -> None:
+    """Restores the max-heap property violated by a single node.
+
+    Implements:
+        Max-Heapify
+
+    Args:
+        A: the array representing a max-heap in which the max-heap property is violated by a single node
+        i: the index of the node in A that is not larger than either of its children
+    """
+    l = left(i)
+    r = right(i)
+    if l <= A.heap_size and A[l] > A[i]:
+        largest = l
+    else:
+        largest = i
+    if r <= A.heap_size and A[r] > A[largest]:
+        largest = r
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        max_heapify(A, largest)
